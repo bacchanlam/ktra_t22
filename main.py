bfe45766569f92347f753b05595126235ec9e879
Bắc <ldbac01032005@gmail.com>
2026-03-09 20:54:09 +0700

    ok 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def sum(a, b):
     return a + b
 
 if __name__ == "__main__":
-    a = 5
-    b = 10
+    a = 10
+    b = 30
     result = sum(a, b)
     print(f"The sum of {a} and {b} is: {result}")
